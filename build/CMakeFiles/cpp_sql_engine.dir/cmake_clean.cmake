6e7edd927ab77db6872dcd321279744237a40036
Antonio Santoro <a.santoro1993@gmail.com>
2020-12-06 14:21:07 +0100

    vscode setup files

---
+++ build/CMakeFiles/cpp_sql_engine.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../lexer.cpp"
+  "../parser.cpp"
+  "../parser.hpp"
+  "CMakeFiles/cpp_sql_engine.dir/lexer.cpp.obj"
+  "CMakeFiles/cpp_sql_engine.dir/main.cpp.obj"
+  "CMakeFiles/cpp_sql_engine.dir/parser.cpp.obj"
+  "cpp_sql_engine.exe"
+  "cpp_sql_engine.exe.manifest"
+  "cpp_sql_engine.pdb"
+  "libcpp_sql_engine.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_sql_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cpp_sql_engine.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cpp_sql_engine.
+# This may be replaced when dependencies are built.
